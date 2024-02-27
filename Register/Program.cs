d18e32325671229c32a6d418900b399bf6c148f1
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-02-27 15:38:57 +0100

    introduced loop for input, added printing of the list of empoloyees

--- Register/Program.cs
+++ Register/Program.cs
@@ -1,19 +1,33 @@
 using namespace Registry;
 
 
-
 public static void Main(string[] args)
 {
-    Console.WriteLine("Name: ");
-    // Read input
-    Console.WriteLine("Salary: ");
-    // Read input
-
-    Employee emp = new Employee(nameEmployee, salaryEmployee);
-
+    bool isComplete = false;
     List<Employee> registryList = new List<Employee>();
+    string input_name = "";
+    double input_salary = 0.0;
 
-    registryList.Add(emp);
+    while (!isComplete)
+    {
+        Console.WriteLine("Name: ");
+        Console.ReadLine(input_name);
+        Console.WriteLine("Salary: ");
+        Console.ReadLine(input_salary);
 
+        char answer = 'n';
+        Console.WriteLine("Add more? (y/n)");
+        Console.ReadLine(answer);
+        answer == 'y' ? isComplete = true : isComplete = false;
+
+        Employee emp = new Employee(input_name, input_salary);
+        registryList.Add(emp);
+
+    }
+
+    foreach (var emp in registryList)
+    {
+        Console.WriteLine(emp.toString());
+    }
 
 }
