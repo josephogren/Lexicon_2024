d49f89ea61660c2e17f37b38469335ba2e756522
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-02-27 15:12:27 +0100

    added Employee class

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Employee
+{
+	string Name = "";
+	double Salary = 0;
+
+	public Employee()
+	{
+		Name = string.Empty;
+	}
+
+	public Employee(string name, double salary)
+	{
+		Name = name;
+		Salary = salary;
+	}
+}
