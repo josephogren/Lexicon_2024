a00b27d6419bb537b7fedf2da7f5c4dc42bae5ab
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-03-05 16:11:56 +0100

    added procedures for Menu Options

--- Exercise_2/Exercise2/FlowControl/Program.cs
+++ Exercise_2/Exercise2/FlowControl/Program.cs
@@ -5,7 +5,12 @@ internal class Program
 
     public static void Main(string[] args)
     {
-        System.Console.WriteLine(   "Välkommen till mitt program!");
+
+        void runOption1() { Console.WriteLine("OPTION 1"); }
+        void runOption3() { Console.WriteLine("OPTION 3"); }
+
+
+        System.Console.WriteLine("Välkommen till mitt program!");
         bool flag = true;
         while (flag)
         {
@@ -13,28 +18,36 @@ internal class Program
             Console.WriteLine("Skriv in siffror..");
             var input_str = Console.ReadLine();
             Console.WriteLine(input_str);
-    
+
             var parsedInputString = int.TryParse(input_str, out var result) ? result : 0;
-    
-            switch (parsedInputString) 
-            {     
-        
-            case 0:
-                flag = false;
-                break;
-            case 1:
-                Console.WriteLine("Du valde alternativ 1");
-                break;
-            case 2:
-                Console.WriteLine("Räkna ut Totalpris");
-                break;
-            case 3:
-                Console.WriteLine("Du valde alternativ 3");
-                break;
-            default:
-                Console.WriteLine("Felaktig inmatning!");
-                break;
-        }}
-    
+
+            switch (parsedInputString)
+            {
+
+                case 0:
+                    flag = false;
+                    break;
+                case 1:
+                    //Console.WriteLine("Du valde alternativ 1");
+                    runOption1();
+                    break;
+                case 2:
+                    Console.WriteLine("Räkna ut Totalpris");
+                    break;
+                case 3:
+                    Console.WriteLine("Du valde alternativ 3");
+                    runOption3();
+                    break;
+                default:
+                    Console.WriteLine("Felaktig inmatning!");
+                    break;
+            }
+        }
+
         System.Console.WriteLine("Programmet avslutas..");
-    }}
+
+
+
+    }
+
+}
