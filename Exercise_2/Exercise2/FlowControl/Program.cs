7977a874f5149357c0207e5747e88ec14bf8a5f5
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-03-06 01:03:00 +0100

    restructured switch

--- Exercise_2/Exercise2/FlowControl/Program.cs
+++ Exercise_2/Exercise2/FlowControl/Program.cs
@@ -3,64 +3,77 @@
 internal class Program
 {
 
-    const int JUNIOR_PRICE = 5;
-    const int ADULT_PRICE = 20;
-    const int SENIOR_PRICE = 11;
+    const int JUNIOR_PRICE = 80;
+    const int STANDARD_PRICE = 120;
+    const int SENIOR_PRICE = 90;
 
 
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
         System.Console.WriteLine("Välkommen till mitt program!");
 
-        // Global Variable
+        // Global Variables
         uint gTotalprice = 0;
 
-        // CASES FOR THE SWITCH 
 
-        void runOption1()
+        // CASES FOR THE SWITCH         
+
+        // MENYVAL 1 UNGDOM ELLER PENSIONÄR
+        void menyVal1()
         {
+            Console.Clear();
             Console.WriteLine("Enter age: ");
 
             var input_str = Console.ReadLine();
 
-            var age = int.TryParse(input_str, out var result) ? result : 0;
+            var age = int.TryParse(input_str, out var result) ? result : 0;        
 
-            if (age < 20)
-            {
-                //Console.WriteLine("Age < 20");
-                gTotalprice += JUNIOR_PRICE;
-                Console.WriteLine($"TOTAL PRICE: {gTotalprice}");
-            }
 
-            else if (age < 65)
-            {
-                //Console.WriteLine("Age is between 20 and 64");
-                Console.WriteLine($"TOTAL PRICE: {gTotalprice}");
-                gTotalprice += ADULT_PRICE;
-                Console.WriteLine($"TOTAL PRICE: {gTotalprice}");
-            }
 
-            else
+            if (age >= 20)
             {
-                gTotalprice += SENIOR_PRICE;
-                Console.WriteLine($"TOTAL PRICE: {gTotalprice}");
+                if (age < 65) { Console.WriteLine("Standardpris 120kr"); }
+
+                else { Console.WriteLine("Pensionärspris 90kr"); }
             }
+            else { Console.WriteLine("Ungdomspris: 80kr"); }
 
         }
 
-        void runOption3() { Console.WriteLine("OPTION 3"); }
-
-        void runOption4()
+        void subMenu() 
         {
+            int count = 167;
+            Console.WriteLine("Antal i sällskapet: ");
+            gTotalprice += STANDARD_PRICE;
+            Console.WriteLine($"TICKETS: {count}, TOTAL PRICE: {gTotalprice}");
+        }
+
+        // MENYVAL 2 UPREPA TIO GÅNGER
+        void menyVal2()
+        {
+            Console.Clear();
+            Console.WriteLine("RandomText> ");
+            var input_str = Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(input_str);
+            }
+        }
+
+        // MENYVAL 3 DET TREDJE ORDET
+        void menyVal3()
+        {
+            Console.Clear();
             Console.Write("Skriv Dina Ord.....");
             var input_str = Console.ReadLine();
             Console.WriteLine(input_str);
-            
-            if(!string.IsNullOrEmpty(input_str))
+
+            // Check thtat our input string is not null or empty as well as has 3 or more words
+            if (!string.IsNullOrEmpty(input_str) && input_str.Length >= 3)
             {
                 string[] splitInput = input_str.Split(" ");
                 Console.WriteLine($"Det 3e Ordet: {splitInput[2]}");
-            }                    
+            }
 
         }
 
@@ -68,6 +81,7 @@ internal class Program
 
         while (flag)
         {
+            Console.Clear();
             Console.WriteLine("** HUVUDMENU **");
             Console.WriteLine("Skriv in siffror..");
             var input_str = Console.ReadLine();
@@ -80,21 +94,32 @@ internal class Program
                 case 0:
                     flag = false;
                     break;
+
+
                 case 1:
                     Console.WriteLine("** BUY YOUR TICKETS! **");
-                    runOption1();
+                    menyVal1();
                     break;
+
+
                 case 2:
-                    Console.WriteLine("Räkna ut Totalpris");
+                    Console.WriteLine("Paketpris");
+                    subMenu();
                     break;
+
+
                 case 3:
-                    Console.WriteLine("Du valde fel alternativ 3");
-                    runOption3();
+                    Console.WriteLine("Upprepa 10 ggr");
+                    menyVal2();
                     break;
+
+
                 case 4:
                     Console.WriteLine("Menyval 3: Det tredje ordet");
-                    runOption4();
+                    menyVal3();
                     break;
+
+
                 default:
                     Console.WriteLine("Felaktig inmatning!");
                     break;
@@ -104,5 +129,11 @@ internal class Program
         System.Console.WriteLine("Programmet avslutas..");
         Console.Beep();
     }
+
+}
+
+
+internal class Printer
+{
 
 }
