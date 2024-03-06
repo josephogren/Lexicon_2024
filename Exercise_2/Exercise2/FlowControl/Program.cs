dd4f71bf14069f5bf72f585143b6fc55443eb315
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-03-06 07:02:07 +0100

    renamed Menu methods and introduced Enum for menu options

--- Exercise_2/Exercise2/FlowControl/Program.cs
+++ Exercise_2/Exercise2/FlowControl/Program.cs
@@ -1,11 +1,22 @@
 ﻿namespace FlowControl;
 
+enum MenuOption
+{
+    Exit,
+    BuyTickets,
+    Group,
+    Repeat10Times,
+    ThirdWord
+}
+
 internal class Program
 {
 
     const int JUNIOR_PRICE = 80;
     const int STANDARD_PRICE = 120;
     const int SENIOR_PRICE = 90;
+
+    const string menu = "0 - Exit, 1 - BILJETTER, 2 - Grupp, 3 - X10 GGR, 4 - DET TREDJE ORDET";
 
 
     static void Main(string[] args)
@@ -14,134 +25,139 @@ internal class Program
 
         // Global Variable
         uint gTotalprice = 0;
-
-
-        // CASES FOR THE SWITCH         
-
-        // MENYVAL 1 UNGDOM ELLER PENSIONÄR
-        void menyVal1()
-        {
-            var age = 0;
-            
-            do 
-            { 
-                Printer.PrintMessage("Age: ");
-                age = Printer.PromptNumber();
-
-            } while (age <= 0);     
-
-            // Check if person is an Adult
-            if (age >= 20)
-            {  // Make sure person is not a Senior
-                if (age < 65) { 
-                    Printer.PrintMessage("Standardpris 120kr");
-                    gTotalprice += STANDARD_PRICE;
-                } 
-                else 
-                { 
-                    Printer.PrintMessage("Pensionärspris 90kr");
-                    gTotalprice += SENIOR_PRICE;
-                }
-            } 
-            
-            else 
-            { 
-                Printer.PrintMessage("Ungdomspris: 80kr"); 
-                gTotalprice += JUNIOR_PRICE;
-            }
-        }
-
-        void subMenu()
-        {
-            gTotalprice = 0;
-
-            Printer.PrintMessage("Antal i sällskapet: ");
-
-            var input_str = Printer.PromptText();
-            var nPeople = int.TryParse(input_str, out var result) ? result : 0;
-
-            // Run Menu option 1 (Ticket Purchase) for each person
-            for (int p = 1; p <= nPeople; p++)
-            {
-                menyVal1();
-            }
-
-            Printer.PrintMessage($"Antal personer: {nPeople}  TOTALPRIS: {gTotalprice}");
-
-        }
-
-        // MENYVAL 2 UPREPA TIO GÅNGER
-        void menyVal2()
-        {
-            Printer.Clear();
-            Printer.PrintMessage("Enter Random Text> ");
-            var input_str = Printer.PromptText();
-            Printer.Print10(input_str);
-        }
-
-        // MENYVAL 3 DET TREDJE ORDET
-        void menyVal3()
-        {
-            Printer.Clear();
-            Printer.PrintMessage("Skriv Dina Ord.....");
-            string input_str = Printer.PromptText();
-            string[] inputStrArr = input_str.Split(" ");
-            Printer.PrintMessage(input_str);
-
-            // Check thtat our input string is not null or empty as well as has 3 or more words
-            if (!string.IsNullOrEmpty(input_str) && inputStrArr.Length >= 3)
-            {
-                string[] splitInput = input_str.Split(" ");
-                Printer.PrintMessage($"Det 3e Ordet: {splitInput[2]}");
-            }
-
-        }
-
+        
         bool flag = true;
 
         while (flag)
         {
-            Printer.PrintMessage("0 - Exit, 1 - BILJETTER, 2 - Grupp, 3 - X10 GGR, 4 - DET TREDJE ORDET");
+            Printer.PrintMenu(menu);
 
-            var input_str = Console.ReadLine();
-            Printer.PrintMessage(input_str);
-
-            var parsedInputString = int.TryParse(input_str, out var result) ? result : 0;
-
-            switch (parsedInputString)
+            if (!Enum.TryParse<MenuOption>(Console.ReadLine(), out var menuOption))
             {
-                case 0:
+                Printer.PrintMessage("Invalid input. Please enter a number corresponding to a menu option.");
+                continue;
+            }
+
+            //var input_str = Console.ReadLine();
+            //Printer.PrintMessage(input_str);
+            
+
+            switch (menuOption)
+            {
+                case MenuOption.Exit:
                     flag = false;
                     break;
 
 
-                case 1:
+                case MenuOption.BuyTickets:
                     Printer.PrintMessage("** BUY YOUR TICKETS! **");
-                    menyVal1();
+                    ShowTicketPrices();
                     break;
 
 
-                case 2:
+                case MenuOption.Group:
                     Printer.PrintMessage("Paketpris");
-                    subMenu();
+                    BuyGroupTickets();
                     break;
 
 
-                case 3:
+                case MenuOption.Repeat10Times:
                     Printer.PrintMessage("Upprepa 10 ggr");
-                    menyVal2();
+                    Repeat10Times();
                     break;
 
 
-                case 4:
+                case MenuOption.ThirdWord:
                     Printer.PrintMessage("Menyval 3: Det tredje ordet");
-                    menyVal3();
+                    The3rdWord();
                     break;
 
 
                 default:
                     Printer.PrintMessage("Felaktig inmatning!");
                     break;
+            }
+
+
+            // CASES FOR THE SWITCH         
+
+            // MENYVAL 1 UNGDOM ELLER PENSIONÄR
+            void ShowTicketPrices()
+            {
+                int age;
+                do
+                {
+                    Printer.PrintMessage("Age: ");
+                    age = Printer.PromptNumber();
+
+                } while (age <= 0);
+
+                // Check if person is an Adult
+                if (age >= 20)
+                {  // Make sure person is not a Senior
+                    if (age < 65)
+                    {
+                        Printer.PrintMessage("Standardpris 120kr");
+                        gTotalprice += STANDARD_PRICE;
+                    }
+                    else
+                    {
+                        Printer.PrintMessage("Pensionärspris 90kr");
+                        gTotalprice += SENIOR_PRICE;
+                    }
+                }
+
+                else
+                {
+                    Printer.PrintMessage("Ungdomspris: 80kr");
+                    gTotalprice += JUNIOR_PRICE;
+                }
+            }
+
+            void BuyGroupTickets()
+            {
+                gTotalprice = 0;
+
+                Printer.PrintMessage("Antal i sällskapet: ");
+
+                var input_str = Printer.PromptText();
+                var nPeople = int.TryParse(input_str, out var result) ? result : 0;
+
+                // Run Menu option 1 (Ticket Purchase) for each person
+                for (int p = 1; p <= nPeople; p++)
+                {
+                    ShowTicketPrices();
+                }
+
+                Printer.PrintMessage($"Antal personer: {nPeople}  TOTALPRIS: {gTotalprice}");
+
+            }
+
+            // MENYVAL 2 UPREPA TIO GÅNGER
+            void Repeat10Times()
+            {
+                Printer.Clear();
+                Printer.PrintMessage("Enter Random Text> ");
+                var input_str = Printer.PromptText();
+                Printer.Print10(input_str); // Prints same text 10 times
+            }
+
+            // MENYVAL 3 DET TREDJE ORDET
+            void The3rdWord()
+            {
+                Printer.Clear();
+                Printer.PrintMessage("Skriv Dina Ord.....");
+                string input_str = Printer.PromptText();
+                string[] inputStrArr = input_str.Split(" ");
+                Printer.PrintMessage(input_str);
+
+                // Check thtat our input string is not null or empty as well as has 3 or more words
+                if (!string.IsNullOrEmpty(input_str) && inputStrArr.Length >= 3)
+                {
+                    string[] splitInput = input_str.Split(" ");
+                    Printer.PrintMessage($"Det 3e Ordet: {splitInput[2]}");
+                }
+
             }
         }
 
@@ -158,6 +174,8 @@ internal class Printer
     internal static void Clear() { Console.Clear(); }
 
     internal static void PrintMessage(string message) { Console.WriteLine(message); }
+
+    internal static void PrintMenu(string menu) { PrintMessage(menu); }
 
     internal static int PromptNumber()
     {
