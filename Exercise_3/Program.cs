ad2f4d7e1d06c847e4d74e188aa8d83672772d38
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-03-10 18:21:43 +0100

    created project

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Lexicon .Net 2024 - Exercise 3 (FLOW CONTROL)
+// Author: Joseph Ogren   Date: 2024-03-10
+////////////////////////////////////////////
+
+static void Main(string[] args)
+{
+    Console.WriteLine("*** EXERCISE 3 ***");
+
+
+    Console.Beep();
+}
