0a9ff1aa4df6701e2a29a76d8801780196f805f6
Joseph Joseph <joseph.ar.ogren@outlook.com>
2024-03-11 12:01:20 +0100

    created new vs solution (Exercise3_new)

---
+++ Exercise_3/Exercise3/Exercise3_new/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_new
+{
+    internal class Person
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        //public int Birth { get; set; }
+        //public int Gender { get; set; }
+
+
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            string person_str = $"Name: {Name};  Age: {Age}";
+            return person_str;
+        }
+    }
+}
---
+++ Exercise_3/Exercise3/Exercise3_new/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** EXERCISE 3 ***");
+
+            Person p = new Person(18, "Alice");
+
+            Console.WriteLine(p);
+
+            // *pause program*
+            Console.Read();
+            Console.Beep();
+        }
+    }
+}
