017226d63f9bd863fb526d64949e5022c3c7e54a
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-25 12:49:37 +0100

    addec class Vehicle and Garage

---
+++ Exercise_5/Garage/Garage/Models/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Remoting.Channels;
+
+namespace Garage.Models
+{
+    internal enum carPaint
+    {
+        None = 0,   
+        White, Black, Red, Green, Blue    
+    }
+
+    internal abstract class Vehicle
+    {
+        public int? registrationNumber { get; set; }
+        public carPaint color { get; set; }    
+        public int nWheels { get; set; }
+
+        public Vehicle()
+        {
+            this.registrationNumber= null;
+            this.color = carPaint.None;
+            this.nWheels= 4;
+        }
+
+    }
+}
