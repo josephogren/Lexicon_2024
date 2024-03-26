a77597fa63f569a543db9782bb060d5b0869c326
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-26 14:13:35 +0100

    added more Vehicle types (Car, Boat, Airplane)

--- Exercise_5/Garage/Garage8/Models/Garage.cs
+++ Exercise_5/Garage/Garage8/Models/Garage.cs
@@ -4,22 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Garage.Models
 {
-    internal class Garage<V>
+    internal abstract class Garage
     {
 
-        private readonly V _value;
-        private readonly string _name;
-        private readonly string _description;
-        private readonly string _type;
+        //private readonly V _value;
+        //private readonly string _name;
+        //private readonly string _description;
+        //private readonly string _type;
         
 
-        private List<V> listOfVehicles = new List<V>();
+        private List<Vehicle> listOfVehicles = new List<Vehicle>();
 
         public Garage()
         {
-                
+                listOfVehicles = new List<Vehicle>();
+
+            
         }
+
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            listOfVehicles.Add(vehicle);
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            listOfVehicles.Remove(vehicle);
+        }
+
+
+        public void ShowAllVehicles()
+        {
+            foreach (Vehicle vehicle in listOfVehicles)
+            {
+                vehicle.Print();
+            }
+
+            
+        }
+
     }
 }
--- Exercise_5/Garage/Garage8/Models/Vehicle.cs
+++ Exercise_5/Garage/Garage8/Models/Vehicle.cs
@@ -1,13 +1,16 @@
 ﻿//using System.Runtime.Remoting.Channels;
 
+using System.Drawing;
+using System.Reflection;
+
 namespace Garage.Models
 {
 
     // The different colors a vehicle may have (including None for no color)
     internal enum vColor
     {
-        None = 0,   
-        White, Black, Red, Green, Blue    
+        None = 0,
+        White, Black, Red, Green, Blue
     }
 
 
@@ -16,11 +19,11 @@ namespace Garage.Models
     ///  
     /// 
     /// </summary>
-    internal abstract class Vehicle
+    internal  class Vehicle
     {
         public int? VIN { get; set; }           /* Vehicle Identification Number (unique int) */
-        
-        public string? vMake {  get; set; }      /* The make of the vechicle  */       
+
+        public string? vMake { get; set; }      /* The make of the vechicle  */
         public string? vModel { get; set; }      /* The model of the vehicle  */
         public string? RegPlate { get; set; }    /* Registratin Plate (string) */
 
@@ -31,14 +34,30 @@ namespace Garage.Models
 
         public Vehicle()
         {
-            this.VIN= null;
+            this.VIN = null;
             this.vColor = Models.vColor.None;
-            this.nWheels= 4;
+            this.nWheels = 4;
         }
 
         public void Print()
         {
-            System.Console.WriteLine($"REGNR:{registrationNumber},  COLOR:{this.color}, WHEELS:{nWheels}");
+            if (VIN != null)
+            {
+                Console.Write("VEHICLE ID="); Console.WriteLine(VIN);
+
+                string strVehicle = string.Empty;
+
+                strVehicle =
+                    $"REGNR:{RegPlate},  " +
+                    $"COLOR:{this.vColor}, " +
+                    $"WHEELS:{nWheels}" +
+                    $"MAKE:{vMake}" +
+                    $"MODEL:{vModel}";
+
+                System.Console.WriteLine(strVehicle); 
+            }
+
+            else Console.WriteLine($"VEHICLE DOES NOT EXIST!");
         }
 
     }
@@ -49,25 +68,42 @@ namespace Garage.Models
     {
         public Car()
         {
-            this.registrationNumber= null;
+            this.VIN = null;
         }
 
-            
+        public Car(string model, string make, vColor color)
+        {
+            this.nWheels = 4;
+                    
+        }
+
+
     }
 
-    internal class Boat : Vehicle 
+    internal class Boat : Vehicle
     {
         public Boat()
         {
-            
+            this.VIN = null;
+        }
+
+        public Boat(string make, string model, vColor color) 
+        { 
+            this.nWheels = 0; 
+            this.vMake = make;
+            this.vColor = color;
+            this.vModel = model;
         }
     }
 
-    internal class Airplane : Vehicle 
+    internal class Airplane : Vehicle
     {
-        public Airplane()
+        public Airplane(string make, string model, vColor color)
         {
-            
+            this.nWheels = 8;
+            this.vMake = make;
+            this.vColor = color;
+            this.vModel = model;
         }
     }
 
