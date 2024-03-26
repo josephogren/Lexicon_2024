712a6cfa1e72bce769fa9b6db0cd68281d1bbb16
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-26 14:14:47 +0100

    created [Handlers] /GarageHandler

---
+++ Exercise_5/Garage/Garage8/Handlers/GarageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage8.Handlers
+{
+    internal class GarageHandler
+    {
+
+        public GarageHandler() { }
+        public GarageHandler(string name)
+        {
+                
+        }   
+
+        
+    }
+}
