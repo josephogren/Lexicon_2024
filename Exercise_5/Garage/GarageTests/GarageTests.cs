47a64d5a190de34bbf5e2f9805e2098acaa2635f
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-26 11:06:26 +0100

    xUnit Test project was added

---
+++ Exercise_5/Garage/GarageTests/GarageTests.cs
@@ -0,0 +1,24 @@
+namespace GarageTests
+{
+    public class GarageTests
+    {
+
+
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+
+        }
+
+        [Fact]
+        public void Test2() 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+                
+
+    }
+}
