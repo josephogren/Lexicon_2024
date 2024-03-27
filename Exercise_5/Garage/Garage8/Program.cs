fc4193fc07724a0529c9783e3b88cb6464c546e4
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-27 09:17:27 +0100

    refactoring Program

--- Exercise_5/Garage/Garage8/Program.cs
+++ Exercise_5/Garage/Garage8/Program.cs
@@ -1,81 +1,112 @@
-﻿using Garage.Models;
-using System;
-
-namespace Garage
+﻿namespace GarageApp
 {
+
+
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            const int exercise = 5; // the current exercise
-            string input = null;
-            bool quit = false;
-            int result = 0;
-            string title = $"Lexicon 2024 .Net - Övning{exercise} (Garage)";
 
+
+
+        // ## DEFINES AND CONSTS ##
+        const int exercise = 5; // the current exercise
+        string input = null;
+        bool quit = false;
+        int result = 0;
+        static string title = $"Lexicon 2024 .Net - Övning{exercise} (Garage)";
+
+
+        // ENVIRONMENT AND DIRECTORY INFO
+        static string CurrentWorkingDirectory = Environment.CurrentDirectory;
+        static string SolutionFolder = Directory.GetParent(CurrentWorkingDirectory).Parent.Parent.Parent.FullName;
+        static string MainProjectFolder = Directory.GetParent(CurrentWorkingDirectory).Parent.Parent.FullName;
+
+        string[] directories = Directory.GetDirectories(SolutionFolder);
+        static FileInfo[] fileList = Directory.GetParent(MainProjectFolder).GetFiles();
+        string[] projectFiles = Directory.EnumerateFiles(MainProjectFolder).ToArray();
+
+
+        static void Initialize()
+        {
             ////initialize
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.SetWindowSize(800, 600);
             Console.Title = title;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+           
 
 
             ////run
 
-            do
-            {
-                /* start-loop */
-                Console.WriteLine("** EXERCISE 5 - GARAGE **");
-                Console.WriteLine("Enter Choice (0 to quit): ");
-
-                input = Console.ReadLine();
-                /* pause-loop */
-
-                int.TryParse(input, out result);
-                //result == 0 ? quit = true : quit = false;
-                if (result == 0) { quit = true; }
-
-                /* continue-loop */
-
-                switch (result)
-                {
-                    case 1:
-                        Console.WriteLine("Car Paint WHITE selected");
-                        Vehicle v1 = new Car { color = carPaint.White };
-                        v1.Print();
-                        break;
-                    case 2:
-                        Console.WriteLine("Car Paint RED selected");
-                        Vehicle v2 = new Car { color = carPaint.Red };
-                        v2.Print();
-                        break;
-                    case 3:
-                        Console.WriteLine("Car Paint BLUE selected");
-                        Vehicle v3 = new Car { color = carPaint.Blue };
-                        v3.Print();
-                        break;
-                    case 4:
-                        Console.WriteLine("Car Paint GREEN selected");
-                        Vehicle v4 = new Car { color = carPaint.Green };
-                        v4.Print();
-                        break;
-                    case 99:
-                        Console.WriteLine("99");
-                        break;
-                    default:
-                    
-                        Console.WriteLine("DEFAULT Car Paint BLACK selected");
-                        Vehicle v = new Car { color = carPaint.Black };
-                        v.Print();
-                        break;
-                        
-                }
 
 
+            foreach (FileInfo file in fileList) { Console.WriteLine(file); }
 
 
-                /* end-loop */
-            } while (!quit);
+            //do
+            //{
+            //    /* start-loop */
+            //    Console.WriteLine("** EXERCISE 5 - GARAGE **");
+            //    Console.WriteLine("Enter Choice (0 to quit): ");
+
+            //    input = Console.ReadLine();
+            //    /* pause-loop */
+
+            //    int.TryParse(input, out result);
+            //    //result == 0 ? quit = true : quit = false;
+            //    if (result == 0) { quit = true; }
+
+            //    /* continue-loop */
+
+            //    switch (result)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Car Paint WHITE selected");
+            //            Models.Vehicle v1 = new Car { vColor = Models.vColor.White };
+            //            v1.Print();
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Car Paint RED selected");
+            //            Models.Vehicle v2 = new Car { vColor = Models.vColor.Red };
+            //            v2.Print();
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Car Paint BLUE selected");
+            //            Models.Vehicle v3 = new Car { vColor = Models.vColor.Blue };
+            //            v3.Print();
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Car Paint GREEN selected");
+            //            Models.Vehicle v4 = new Car { vColor = Models.vColor.Green };
+            //            v4.Print();
+            //            break;
+            //        case 99:
+            //            Console.WriteLine("99");
+            //            break;
+            //        default:
+
+            //            Console.WriteLine("DEFAULT Car Paint BLACK selected");
+            //            Models.Vehicle v = new Car { vColor = Models.vColor.Black };
+            //            v.Print();
+            //            break;
+
+            //    }
+
+
+
+
+            //    /* end-loop */
+            //} while (!quit);
 
 
 
@@ -84,5 +115,17 @@ namespace Garage
             Console.In.Dispose();
             Console.Beep();
         }
+
+
+
+
+
     }
+
+
+
+
+
+
+
 }
