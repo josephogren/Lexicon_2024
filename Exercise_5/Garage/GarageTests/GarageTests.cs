2620c54906a8a31dbf6719453ab0570f5a718ba5
JavaJoseph <joseph.ar.ogren@outlook.com>
2024-03-27 09:27:38 +0100

    additional tests in GarageTests

--- Exercise_5/Garage/GarageTests/GarageTests.cs
+++ Exercise_5/Garage/GarageTests/GarageTests.cs
@@ -4,27 +4,56 @@ namespace GarageTests
     {
 
 
+        static string CurrentWorkingDirectory = Environment.CurrentDirectory;
+        static string SolutionFolder = Directory.GetParent(CurrentWorkingDirectory).Parent.Parent.Parent.FullName;
+        static string MainProjectFolder = Directory.GetParent(CurrentWorkingDirectory).Parent.Parent.FullName;
+
+        string[] directories = Directory.GetDirectories(SolutionFolder);
+        //FileInfo[] fileList = Directory.GetParent(MainProjectFolder).GetFiles();
+        //File projectFiles = Directory.EnumerateFiles(MainProjectFolder);
+        string[] projectFiles = Directory.EnumerateFiles(MainProjectFolder).ToArray();
+
 
         [Fact]
-        public void Test1()
+        public void TestofGary()
         {
-            Assert.Equal(1, 1);
+            GarageApp.Handlers.GarageHandler gh = new GarageApp.Handlers.GarageHandler();
 
+            Garage garage = gh.Build();
+
+            Assert.NotNull(garage);        
+            
+        }
+
+
+        [Fact]
+        public void number_of_wheels_is_in_range_0_to_16()
+        {
+            GarageApp.Models.Car vehicle = new GarageApp.Models.Car();
+            int wheels = vehicle.nWheels;
+
+            Assert.InRange<int>(wheels, 0, 16);
         }
 
         [Fact]
-        public void Test2() 
-        { 
-            throw new NotImplementedException(); 
+        public void no_boat_should_have_any_wheels_shouldit()
+        {
+            GarageApp.Models.Boat vehicle = new GarageApp.Models.Boat();
+            int wheels = vehicle.nWheels;
+
+            Assert.Equal(wheels, 0);
         }
 
         [Fact]
-        public void CheckThatGarageHandlerExists() 
-        { 
-            throw new NotImplementedException(); 
+        public void a_boat_should_have_a_proppeller_tho()
+        {
+            GarageApp.Models.Boat vehicle = new GarageApp.Models.Boat ();
+
+            bool hasRudder = vehicle.hasRudder;
+
+            Assert.True(hasRudder);
         }
 
-                
 
     }
 }
